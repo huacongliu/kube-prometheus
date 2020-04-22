7ef46a7ab0483c54db984bbebd6d98ba3ec50066
Your Name <you@example.com>
2020-04-22 17:38:08 +0800

    update etcd

--- manifests/add/etcd/install.sh
+++ manifests/add/etcd/install.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+read -p "please input etcd_master1_ip:" etcd_master1_ip
+read -p "please input etcd_master2_ip:" etcd_master2_ip
+read -p "please input etcd_master3_ip:" etcd_master3_ip
+sed -i "s/10.32.3.57/${etcd_master1_ip}/g" prometheus-etcdService.yaml
+sed -i "s/10.32.3.57/${etcd_master2_ip}/g" prometheus-etcdService.yaml
+sed -i "s/10.32.3.57/${etcd_master3_ip}/g" prometheus-etcdService.yaml
 kubectl delete secret etcd-certs -n monitoring
 kubectl -n monitoring create secret generic etcd-certs --from-file=/etc/kubernetes/pki/etcd/healthcheck-client.crt --from-file=/etc/kubernetes/pki/etcd/healthcheck-client.key --from-file=/etc/kubernetes/pki/etcd/ca.crt
 kubectl delete pod prometheus-k8s-0 -n monitoring
