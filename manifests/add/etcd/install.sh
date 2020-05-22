c0cf379dda5eb92828d2f0ca0d51f4566f72fcb6
chianboy007 <305837438@qq.com>
2020-05-22 16:51:35 +0800

    install.sh

--- manifests/add/etcd/install.sh
+++ manifests/add/etcd/install.sh
@@ -7,8 +7,8 @@ sed -i "s/10.32.3.57/${etcd_master2_ip}/g" prometheus-etcdService.yaml
 sed -i "s/10.32.3.57/${etcd_master3_ip}/g" prometheus-etcdService.yaml
 kubectl delete secret etcd-certs -n monitoring
 kubectl -n monitoring create secret generic etcd-certs --from-file=/etc/kubernetes/pki/etcd/healthcheck-client.crt --from-file=/etc/kubernetes/pki/etcd/healthcheck-client.key --from-file=/etc/kubernetes/pki/etcd/ca.crt
-kubectl delete pod prometheus-k8s-0 -n monitoring
-kubectl delete pod prometheus-k8s-1 -n monitoring
+kubectl delete pod prometheus-k8s-0 -n monitoring >/dev/null 2>&1
+kubectl delete pod prometheus-k8s-1 -n monitoring >/dev/null 2>&1
 kubectl apply -f ../nfs-client/prometheus-prometheus.yaml
 kubectl apply -f prometheus-serviceMonitorEtcd.yaml
 kubectl apply -f prometheus-etcdService.yaml
