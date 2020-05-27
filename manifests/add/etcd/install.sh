2a085705d7ae4327ee1b0de2b86776decbae24e4
chianboy007 <305837438@qq.com>
2020-05-27 20:02:11 +0800

    修复etcd监控bug

--- manifests/add/etcd/install.sh
+++ manifests/add/etcd/install.sh
@@ -6,7 +6,7 @@ read -p "please input etcd_master3_ip:" etcd_master3_ip
 sed -i "s/10.32.3.57/${etcd_master1_ip}/g" prometheus-etcdService.yaml
 sed -i "s/10.32.3.58/${etcd_master2_ip}/g" prometheus-etcdService.yaml
 sed -i "s/10.32.3.59/${etcd_master3_ip}/g" prometheus-etcdService.yaml
-kubectl delete secret etcd-certs -n monitoring
+kubectl delete secret etcd-certs -n monitoring >/dev/null 2>&1
 kubectl -n monitoring create secret generic etcd-certs --from-file=/etc/kubernetes/pki/etcd/healthcheck-client.crt --from-file=/etc/kubernetes/pki/etcd/healthcheck-client.key --from-file=/etc/kubernetes/pki/etcd/ca.crt
 kubectl delete pod prometheus-k8s-0 -n monitoring >/dev/null 2>&1
 kubectl delete pod prometheus-k8s-1 -n monitoring >/dev/null 2>&1
