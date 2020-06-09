27aee49c0900e71cf0f3dd061d8aa41cba1999b0
chianboy007 <305837438@qq.com>
2020-06-09 19:03:43 +0800

    update wechat

--- manifests/add/alert/wechat/install.sh
+++ manifests/add/alert/wechat/install.sh
@@ -1,5 +1,5 @@
-kubectl delete configmap wechat  -n monitoring
-kubectl create configmap wechat --from-file=wechat.tmpl -n monitoring
+#kubectl delete configmap wechat  -n monitoring
+#kubectl create configmap wechat --from-file=wechat.tmpl -n monitoring
 
 kubectl delete secret alertmanager-main -n monitoring
 kubectl create secret generic alertmanager-main --from-file=alertmanager.yaml  --from-file=wechat.tmpl  -n monitoring
